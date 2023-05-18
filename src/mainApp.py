5a8ecbbf62f300af2894681adf24a960839f55e9
oirams <oiram.1096.siul@gmail.com>
2023-05-17 18:16:15 -0600

    tested the navbar config

--- src/mainApp.py
+++ src/mainApp.py
@@ -1,14 +1,76 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.LUX])
+# navbar = dbc.NavbarSimple(
+#     id="navbar",
+#     brand_href="/",
+#     children=[
+#         # Elemento de navegación nav-link para estilo y mx-3 para un margen de 0.75rem en ambos lados.
+#         dbc.NavItem(
+#             dcc.Link(
+#                 "Inicio",
+#                 href="/",
+#                 className="nav-link mx-3",
+#                 style={"whiteSpace": "nowrap"},
+#             )
+#         ),
+#         dbc.NavItem(
+#             dcc.Link(
+#                 "Análisis Exploratorio de Datos (EDA)",
+#                 href="/eda",
+#                 className="nav-link mx-3",
+#                 style={"whiteSpace": "nowrap"},
+#             )
+#         ),
+#         dbc.NavItem(
+#             dcc.Link(
+#                 "Análisis de Componentes Principales (PCA)",
+#                 href="/pca",
+#                 className="nav-link mx-3",
+#                 style={"whiteSpace": "nowrap"},
+#             )
+#         ),
+#         dbc.NavItem(className="ml-auto"),
+#     ],
+#     color="red",
+#     dark=False,
+#     sticky="top",
+# )
 
-app.layout = html.Div([
-    html.Div(
-        className="app-header",
-        children=[
-            html.Div('MineWizard', className="app-header--title")
-        ]
-    ),
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("EDA", href="#"), className="example"),
+        dbc.NavItem(dbc.NavLink("PCA", href="#")),        
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Clasificación", header=True),
+                dbc.DropdownMenuItem("Árbol", href="#"),
+                dbc.DropdownMenuItem("Bosque", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Clasificación",
+        ),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Pronóstico", header=True),
+                dbc.DropdownMenuItem("Árbol", href="#"),
+                dbc.DropdownMenuItem("Bosque", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Pronóstico",
+        ),
+    ],
+    brand="MineWizard",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
+
+app.layout = html.Div([dcc.Location(id = "url"),
+    navbar,
     html.Div(
         children=html.Div([
             html.H1('Overview'),
