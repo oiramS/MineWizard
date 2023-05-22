6c94738cf38b2ebedd9d4060d03d42ed0f51f9b6
Brenda-RT <bmonica.rt@gmail.com>
2023-05-21 20:42:23 -0600

    Explicación PCA

--- src/components/PCA.py
+++ src/components/PCA.py
@@ -8,10 +8,19 @@ def render(app: Dash) -> html.Div:
     return html.Div(
         children=html.Div([
             html.H1('Análisis de Componentes Principales'),
+            html.P('''
+                
+                El Análisis de Componentes Principales (en inglés Principal Component Analysis, PCA), 
+                es un método estadístico que nos permite reducir la dimensionalidad de los datos con los
+                que estamos trabajando. Se utiliza cuando queremos elegir un menor número de predictores 
+                para pronosticar una variable objetivo, o para comprenderlos de una forma más simple (Morán, 2022).
+            '''),
             html.Div('''
-
-                This is an example of a simple Dash app with
-                local, customized CSS.
+                
+                El Análisis de Componentes Principales (en inglés Principal Component Analysis, PCA), 
+                es un método estadístico que nos permite reducir la dimensionalidad de los datos con los
+                que estamos trabajando. Se utiliza cuando queremos elegir un menor número de predictores 
+                para pronosticar una variable objetivo, o para comprenderlos de una forma más simple.
             '''    ,className="example")
 
         ] )
