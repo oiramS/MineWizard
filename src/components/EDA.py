e3e4968ceb198ab651cc31dd111b6db069b019db
Brenda-RT <bmonica.rt@gmail.com>
2023-05-22 01:25:50 -0600

    Descripción general y definición EDA

--- src/components/EDA.py
+++ src/components/EDA.py
@@ -8,6 +8,9 @@ def render(app: Dash) -> html.Div:
     return html.Div(
         children=html.Div([
             html.H1('Análisis Exploratorio de Datos'),
+            html.P('''El anáisis exploratorio de datos (Exploratory Data Análisis, EDA) es
+                una serie de pasos para analizar sets de datos 
+                y extraer sus características principales (Granados-López, 2021)'''),
             html.Div('''
 
                 This is an example of a simple Dash app with
--- src/components/overview.py
+++ src/components/overview.py
@@ -7,9 +7,9 @@ def render(app: Dash) -> html.Div:
     '''
     return html.Div(
         children=html.Div([
-            html.H1('Overview'),
+            html.H1('Descripción general'),
             html.Div('''
-
+                MineWizard es una herramienta creada para ...
                 This is an example of a simple Dash app with
                 local, customized CSS.
             '''    ,className="example")
